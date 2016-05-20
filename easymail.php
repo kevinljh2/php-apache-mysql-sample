d93f7f0519a8b421d0f8056c62072f8dba74a565
kevinljh2 <kevinljh2@hotmail.com>
2016-05-20 20:29:23 +0800

    Update easymail.php

--- easymail.php
+++ easymail.php
@@ -2,8 +2,8 @@
 ini_set('date.timezone','Asia/Shanghai');
 $to = "2971282988@qq.com";
 $subject =date('H:i:s');
-$message =$subject;
+$message =date('H:i:s');
 $from = "kevinljh6@163.com";
 mail($to,$subject,$message,$headers);
-echo "Mail Sent.".date('H:i:s');
+echo "Mail Sent-".date('H:i:s');
 ?>
