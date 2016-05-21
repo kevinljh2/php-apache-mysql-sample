9cd2a486f446039160b45afae465e615f2f101be
kevinljh2 <kevinljh2@hotmail.com>
2016-05-21 18:11:29 +0800

    Update long.php

--- long.php
+++ long.php
@@ -1,17 +1,4 @@
 <?
-ini_set('date.timezone','Asia/Shanghai');
-error_reporting(E_ALL ^ E_NOTICE);
-print('<!DOCTYPE HTML>
-<html>
-<head>
-    <title>Long</title>
-	<meta http-equiv=Content-Type content="text/html;charset=utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no" />
-    <meta http-equiv="refresh" content="15">
-</head>
-<body bgcolor="black">
-<font color="#C0C0C0">');
-$mailbool=false;
 $url="http://lt.bbbaiu.com/pt/mem/ajax/casino/external/public/3819831?lang=zh-cn";
 $ch = curl_init(); 
 curl_setopt ($ch, CURLOPT_URL, $url); 
@@ -32,25 +19,10 @@ $count=$namearr1[1];
 $out1=$name1.",".$nametz.",".$count; 
 $out2.=$out1."<br>";
 
-if ($count>=9 && $mailbool==false)
-{
-$mailbool=true;
 }
 
-}
 $out3=json_decode('"'.$out2.'"');
 echo $out3;
 echo "</body></html>";
-
-if ($mailbool==true)
-{
-$mailbool=false;
-$urla="http://kevinljh2-wp.daoapp.io/sendmail6.php?text=".$out3;
-$cha = curl_init(); 
-curl_setopt ($cha, CURLOPT_URL, $urla); 
-curl_setopt ($cha, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt ($cha, CURLOPT_CONNECTTIMEOUT,10); 
-$dataa = curl_exec($cha); 
-}
 
 ?>
